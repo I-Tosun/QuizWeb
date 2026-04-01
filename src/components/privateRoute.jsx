05b7d8192adc71a4e26d7dff5fa6891e9534e324
I-Tosun <Ismail.tosun@ns.nl>
2026-04-01 20:42:45 +0200

    feat: add private route protection for admin pages

--- src/components/privateRoute.jsx
+++ src/components/privateRoute.jsx
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { isAdmin } from "../services/authService";
 
+//PrivateRoute protects admin page form unlogged users
 const PrivateRoute = ({ children }) => {
     if (!isAdmin()) {
         return <Navigate to="/" replace />;
